09062384db412c4970fe5243c6088403acab39ed
Alex Gnatko <gnatko@mail.ru>
2017-07-16 18:01:42 +0800

    Close and activate websites, website archive

--- htdocs/modules/factory/websites/pads/info.php
+++ htdocs/modules/factory/websites/pads/info.php
@@ -17,3 +17,5 @@ if($_GET['activate_website']){
     $emps->db->sql_update_row($this->table_name, $update, "id = ".$this->row['id']);
     $emps->redirect_elink(); exit;
 }
+
+//trying another checkout
