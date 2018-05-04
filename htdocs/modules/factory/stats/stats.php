fc85808b9cb22cce61de99548f2790bdf1cd7a29
Alex Gnatko <agnatko@gmail.com>
2018-05-04 22:54:31 +0800

    Aggregate stats

--- htdocs/modules/factory/stats/stats.php
+++ htdocs/modules/factory/stats/stats.php
@@ -5,6 +5,9 @@ if($emps->auth->credentials("admin")){
     $slst = [];
     $r = $emps->db->query("select * from ".TP."ef_stats");
     while($ra = $emps->db->fetch_named($r)){
+        if(!$ra['ef_website_id']){
+            continue;
+        }
         $stats = $ef->list_stats($ra['id']);
         if(!$slst[$stats['period']]){
             $slst[$stats['period']] = ['period' => $stats['period'], 'websites' => []];
