cc962e8bffa615e34ee070de1701c35205124e8e
Alex Gnatko <agnatko@gmail.com>
2018-05-04 19:40:03 +0800

    Stats pad in websites

--- htdocs/modules/factory/sql/module.sql
+++ htdocs/modules/factory/sql/module.sql
@@ -54,3 +54,37 @@ CREATE TEMPORARY TABLE `temp_ef_heartbeat` (
   KEY `nedt` (`nedt`),    
   KEY `cdt` (`cdt`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+-- table
+CREATE TEMPORARY TABLE `temp_ef_stats` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL DEFAULT '0',
+  `ef_website_id` bigint NOT NULL DEFAULT '0',
+  `status` int NOT NULL,
+  `nedt` bigint NOT NULL,
+  `cdt` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `ef_website_id` (`ef_website_id`),
+  KEY `status` (`status`),
+  KEY `nedt` (`nedt`),
+  KEY `cdt` (`cdt`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+-- table
+CREATE TEMPORARY TABLE `temp_ef_stats_values` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `stats_id` bigint NOT NULL DEFAULT '0',
+  `period` int NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `value` int NOT NULL,
+  `dt` bigint NOT NULL,
+  `cdt` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `stats_id` (`stats_id`),
+  KEY `period` (`period`),
+  KEY `code` (`code`),
+  KEY `value` (`value`),
+  KEY `dt` (`dt`),
+  KEY `cdt` (`cdt`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
---
+++ htdocs/modules/factory/websites/pads/stats.php
@@ -0,0 +1,42 @@
+<?php
+
+global $SET, $ef;
+
+$this->handle_view_row();
+
+$row = $this->row;
+
+if($_GET['enable']){
+    $hb = $emps->db->get_row("ef_stats", "ef_website_id = ".$row['id']);
+    if(!$hb){
+        $SET = array();
+        $SET['ef_website_id'] = $row['id'];
+        $SET['status'] = 10;
+        $SET['user_id'] = $emps->auth->USER_ID;
+        $emps->db->sql_insert("ef_stats");
+    }else{
+        $SET = array();
+        $SET['status'] = 10;
+        $emps->db->sql_update("ef_stats", "id = ".$hb['id']);
+    }
+    $emps->redirect_elink();exit();
+}
+
+if($_GET['disable']){
+    $hb = $emps->db->get_row("ef_stats", "ef_website_id = ".$row['id']);
+    if($hb){
+        $SET = array();
+        $SET['status'] = '00';
+        $emps->db->sql_update("ef_stats", "id = ".$hb['id']);
+    }
+    $emps->redirect_elink();exit();
+}
+
+$hb = $emps->db->get_row("ef_stats", "ef_website_id = ".$row['id']);
+
+if($hb){
+    $hb['ntime'] = $emps->form_time($hb['nedt']);
+}
+
+$smarty->assign("stats", $hb);
+
