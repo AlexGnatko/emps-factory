0730c3054aa5a948197c0816b2301335549df22d
Alex Gnatko <agnatko@gmail.com>
2018-05-05 09:30:23 +0800

    DB stats cycle

---
+++ htdocs/modules/test/dbstats/dbstats.php
@@ -0,0 +1,16 @@
+<?php
+$emps->no_smarty = true;
+
+if($emps->auth->credentials("admin")){
+    require_once $emps->page_file_name('_factory,factory.class', 'controller');
+    require_once $emps->page_file_name('_factory,factory_worker.class', 'controller');
+
+    $ef = new EMPS_Factory;
+    $efw = new EMPS_FactoryWorker;
+
+    $ef->load_defaults();
+
+    $efw->db_stats_cycle();
+}else{
+    $emps->deny_access("AdminNeeded");
+}
