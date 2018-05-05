08a09a2bf1e4fc7ab74d5298474423a5690fbda7
Alex Gnatko <agnatko@gmail.com>
2018-05-05 11:54:32 +0800

    Show DB stats

--- htdocs/modules/factory/factory.class.php
+++ htdocs/modules/factory/factory.class.php
@@ -368,7 +368,7 @@ class EMPS_Factory {
     }
 
     public function analyse_db_stats($stat){
-	    $class = $stat['count_star'] / 1000000;
+	    $class = pow($stat['count_star'] / 1000000, 1/3);
 
 	    if($stat['count_star'] > 0){
             $stat['sr_cs'] = $stat['sum_rows'] / $stat['count_star'];
