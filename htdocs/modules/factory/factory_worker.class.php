026fa5dd9dd57f550d9c393a5a08fa41cd98ed01
Alex Gnatko <agnatko@gmail.com>
2022-11-30 02:30:41 +0800

    Services

--- htdocs/modules/factory/factory_worker.class.php
+++ htdocs/modules/factory/factory_worker.class.php
@@ -1108,7 +1108,7 @@ class EMPS_FactoryWorker
 			}
 			$dt = time() + $interval;
 			$emps->db->query("update ".TP."ef_heartbeat set nedt = $dt where id = ".$ra['id']);
-//			exec("curl http://".$hostname_part.".".$ef->defaults['hostname_short']."/heartbeat/ &");
+			echo "curl http://".$hostname."/heartbeat/\r\n";
 			exec("echo \"curl -L http://".$hostname."/heartbeat/\" | at -M now");
 		}
 		
